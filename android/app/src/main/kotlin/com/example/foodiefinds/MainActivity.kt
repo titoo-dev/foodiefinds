69cd0c66bacbbab9140b9fa72b947b440ef5c68d
Titosy Manankasina <dev.titosy@gmail.com>
2024-09-08 19:00:54 +0300

    init project

---
+++ android/app/src/main/kotlin/com/example/foodiefinds/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.foodiefinds
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  //TODO: Implement HomeController
+
+  final count = 0.obs;
+
+  void increment() => count.value++;
+}
